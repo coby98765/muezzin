2332df58bb206e8be90dc4e30406dfaee38681a8
coby98765 <coby98765@gmail.com>
2025-09-07 15:50:01 +0300

    muezzin/retriver close Kafka connection

--- src/retriever/main.py
+++ src/retriever/main.py
@@ -6,12 +6,18 @@ import os
 DIR_PATH = os.getenv("DIR_PATH",r"C:\Users\Yaakov\PycharmProjects\muezzin\data\podcasts")
 EXPORT_TOPIC = os.getenv("EXPORT_TOPIC","podcast_meta")
 
+# open Kafka Connection
 kafka_conn = Kafka()
 kafka_conn.create_producer()
 
 pathlist = Path(DIR_PATH).glob('**/*.wav')
 for path in pathlist:
-    path_in_str = str(path)
     file_metadata = path.stat()
     meta = Podcast(str(path),file_metadata)
     kafka_conn.pub(meta.__dict__(),"podcast_meta")
+
+# close Kafka Connection
+kafka_conn.producer.flush()
+kafka_conn.producer.close()
+print("metadata reports completed.")
+
