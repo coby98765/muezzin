1eda48836048642ee2b01e1b0b9ab28f0445cc81
coby98765 <coby98765@gmail.com>
2025-09-08 15:09:21 +0300

    feat: logging in kafka_conn.py

--- src/utils/kafka_conn.py
+++ src/utils/kafka_conn.py
@@ -1,16 +1,17 @@
 from kafka import KafkaProducer,KafkaConsumer
+from src.utils.logger import Logger
 import os
 import json
+
+# logger setup
+logger = Logger.get_logger(index="kafka_log",name="kafka_connection")
 
 class Kafka:
     def __init__(self):
         self.bootstrap_servers = os.getenv("KAFKA_HOST","localhost:9092")
         self.group_id = os.getenv("GROUP_ID","GROUP_ID")
         self.producer = None
-        # self.create_producer()
         self.consumer = None
-        # self.create_consumer(sub_topic)
-        # self.sub_topic = sub_topic
 
     def create_producer(self):
         try:
@@ -18,9 +19,9 @@ class Kafka:
                 bootstrap_servers=self.bootstrap_servers,
                 value_serializer=lambda v: json.dumps(v).encode("utf-8")
             )
-            print('connected to kafka PUB')
+            logger.info('Kafka.create_producer, connected to kafka PUB')
         except Exception as ex:
-            print(ex)
+            logger.error(f"Kafka.create_producer, Error: {ex}")
             raise Exception(ex)
 
     def create_consumer(self,sub_topic):
@@ -31,24 +32,26 @@ class Kafka:
                 group_id=self.group_id,
                 value_deserializer=lambda v: json.loads(v.decode("utf-8"))
             )
-            print('connected to kafka SUB')
+            logger.info('Kafka.create_consumer connected to kafka SUB')
         except Exception as ex:
-            print(ex)
+            logger.error(f"Kafka.create_consumer, Error: {ex}")
             raise Exception(ex)
 
     def pub(self, data, topic):
         try:
-            print("sending to topic:", topic, data)
             self.producer.send(topic, value=data)
+            logger.info(f"Kafka.pub, sent to topic: {topic}")
         except Exception as ex:
-            print(ex)
+            logger.error(f"Kafka.pub, Error: {ex}")
             raise Exception(ex)
 
     def sub(self):
         try:
             for msg in self.consumer:
                 print("received: ",msg.value,type(msg))
+                logger.info('Kafka.sub, received report.')
+
                 yield msg.value
         except Exception as ex:
-            print(ex)
+            logger.error(f"Kafka.sub, Error: {ex}")
             raise Exception(ex)
