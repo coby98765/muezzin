9a2336c1dd3b065688577616c0b95fb53c2bed1c
coby98765 <coby98765@gmail.com>
2025-09-08 15:32:00 +0300

    feat: logging in persister.mongoDAL.py

--- src/persister/mongoDAL.py
+++ src/persister/mongoDAL.py
@@ -1,7 +1,11 @@
+from src.utils.logger import Logger
 from pymongo import MongoClient ,errors
 from gridfs import GridFS
-
 import os
+
+# logger setup
+logger = Logger.get_logger(index="persister_log",name="persister.mongoDAL.py")
+
 
 class MongoDAL:
     def __init__(self):
@@ -9,38 +13,43 @@ class MongoDAL:
         self.DB_NAME = os.getenv("MONGO_NAME","muezzin")
         self.DB_COLL = os.getenv("MONGO_REPORT_COLL", "podcasts_meta")
         self.DB_COLL = os.getenv("MONGO_FILE_COLL", "podcasts_metadata")
+        client = None
         try:
             client = MongoClient(self.DB_HOST)
-        except Exception as err:
-            print(f"MongoDAL.init Unexpected error: {err}")
+            logger.info(f'MongoDAL.init, connected to MongoDB.')
+        except Exception as e:
+            logger.error(f"MongoDAL.init, Error: {e}")
             raise
         finally:
             client.close()
 
 
     def load_report(self,report):
+        client = None
         try:
             client = MongoClient(self.DB_HOST)
             mydb = client[self.DB_NAME]
             collection = mydb[self.DB_COLL]
             res = collection.insert_one(report)
+            logger.info(f'MongoDAL.load_report, {res.inserted_id} added to mongoDB.')
             return res.inserted_id
-        except errors.ServerSelectionTimeoutError as err:
-            print(f"Server selection timeout: {err}")
+        except errors.ServerSelectionTimeoutError as e:
+            logger.error(f"MongoDAL.load_report, Server selection timeout: {e}")
             raise
-        except errors.ConnectionFailure as err:
-            print(f"Connection failed: {err}")
+        except errors.ConnectionFailure as e:
+            logger.error(f"MongoDAL.load_report, Connection failed: {e}")
             raise
-        except errors.ConfigurationError as err:
-            print(f"Configuration error: {err}")
+        except errors.ConfigurationError as e:
+            logger.error(f"MongoDAL.load_report, Configuration error: {e}")
             raise
-        except Exception as err:
-            print(f"MongoDB.load_report Unexpected error: {err}")
+        except Exception as e:
+            logger.error(f"MongoDAL.load_report, Unexpected error: {e}")
             raise
         finally:
             client.close()
 
     def load_file(self,file_path,file_name):
+        client = None
         try:
             client = MongoClient(self.DB_HOST)
             fs = GridFS(client[self.DB_NAME])
@@ -50,11 +59,11 @@ class MongoDAL:
 
             # store in GridFS
             file_id = fs.put(file_data, _id=file_name, content_type='audio/wav')
-            print(f"WAV file '{file_path}' uploaded successfully with GridFS ID: {file_id}")
+            logger.info(f'MongoDAL.load_file, WAV file "{file_path}" uploaded successfully with GridFS ID: {file_id}.')
             return file_id
         except FileNotFoundError:
-            print(f"Error: WAV file not found at '{file_path}'")
+            logger.error(f"MongoDAL.load_file, Error: WAV file not found at '{file_path}'")
         except Exception as e:
-            print(f"MongoDB.load_file An error occurred: {e}")
+            logger.error(f"MongoDAL.load_file, An error occurred: {e}")
         finally:
             client.close()
